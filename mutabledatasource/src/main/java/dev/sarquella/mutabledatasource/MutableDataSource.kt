64306b437b5df6dd5f41b8c2a9115ea2983670fe
Adrià Sarquella <adrisarquella@gmail.com>
2020-06-15 00:50:09 +0200

    Added Factory for MutableDataSource

--- mutabledatasource/src/main/java/dev/sarquella/mutabledatasource/MutableDataSource.kt
+++ mutabledatasource/src/main/java/dev/sarquella/mutabledatasource/MutableDataSource.kt
@@ -1,5 +1,7 @@
 package dev.sarquella.mutabledatasource
 
+import androidx.paging.DataSource
+
 
 /*
  * Created by Adrià Sarquella on 15/06/2020.
@@ -8,7 +10,12 @@ package dev.sarquella.mutabledatasource
 
 typealias MutateFunction<Original, Mutated> = (List<Original>) -> List<Mutated>
 
+typealias FactoryBuilder<Key, Value> =
+        (MutableDataSource.Config<Key>) -> DataSource.Factory<Key, Value>
+
 class MutableDataSource private constructor() {
+
+    class Factory<Key, Value> internal constructor(private val build: FactoryBuilder<Key, Value>)
 
     class Config<Key> internal constructor(internal val initialKey: Key?) {
         internal var alreadyLoaded: Int = 0
